193cdaf9ca657fb8b95e1167710d1c341965df2b
shamiivan <shamiivan@gmail.com>
2023-01-13 11:53:41 -0500

    added preprocess.py

---
+++ prepocess.py
@@ -0,0 +1,17 @@
+""" 
+Recursive run through dataset and musical pitches such as mfcc, frequencies and map them to desired classification
+class such sadness, happiness, etc...
+"""
+from music21 import converter, score, stream
+from collections import defaultdict, OrderedDict
+from typing import List
+
+
+filename_path=""
+
+# recursive run throught directory and
+def get_musical_data(filename:str):
+    pass
+
+def get_element_by_feature():
+    pass
